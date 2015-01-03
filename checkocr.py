eba1928832800f5c2e0b448d1e77abec2f3027e3
chrisdeveloper <chris@chrisdeveloper.com>
2015-01-03 23:23:51 +0100

    Improve regular expression for checking ocr by using [^\W\d_]  & handle the different cases more carefully

--- checkocr.py
+++ checkocr.py
@@ -32,7 +32,7 @@ class PDF(PdfFileReader):
         return content
 
     def is_ocr(self, max_scanned_pages, test_length):
-        treshold = max_scanned_pages * 2
+        treshold = max_scanned_pages * 3
         ### This "treshold" (see bellow) is needed in order to get a content
         ### sample that is more representive of longer pdfs such as ebooks.
         ### Indeed, those pdfs might have ocr, but only after the first few
@@ -43,7 +43,7 @@ class PDF(PdfFileReader):
             content = self.extract_content(first_page=1,
                                            last_page=max_scanned_pages)
         else:
-            content = self.extract_content(first_page=max_scanned_pages+1,
+            content = self.extract_content(first_page=2*max_scanned_pages+1,
                                            last_page=treshold)
         print("--------------------------------------------------------------")
         warning = "PDF compression, OCR, web optimization using \
@@ -53,26 +53,33 @@ a watermarked evaluation copy of CVISION PDFCompressor\n\n"
         ### check_ouput returns bytes instead of a string. The 'ignore'
         ### option is then used to prevent the UnicodeDecodeError that might
         ### occur as the result of the 'content' bit-string being shortened
-        ### at the wrong place with the 'length' parameter.
+        ### at the wrong place.
             print(Fore.RED + warning + "\n")
             return
         l = test_length
-        if (len(content) <= 4*l or self.num_pages <= treshold):
+        if (len(content) <= 6*l or self.num_pages <= treshold):
             ### Condition deals with the special case of small pdfs or pdfs
-            ### whose content can't be split into 4 parts of length l.
+            ### whose content can't be split into 6 parts of length l (even
+            ### though they might have a high number of pages, which is typical
+            ### of non-ocr ebooks).
             content = content.decode('utf-8', 'ignore')
-            return re.search('\w{4}', content, re.UNICODE)
+            print("Number of Pages : {}".format(self.num_pages))
+            print("Name : {}\n".format(self.name))
+            return re.search('[^\W\d_]{5}', content, re.UNICODE)
             ### the re.UNICODE flag is needed for Python 2.7
 
-        test_contents = [content[:l],
-                         content[l: 2*l],
-                         content[:-2*l],
-                         content[:-l]]
+        test_contents = [content[0:l],
+                         content[l:2*l],
+                         content[2*l:3*l],
+                         content[-3*l:-2*l],
+                         content[-2*l:-l],
+                         content[-l:]]
         test_contents = (c.decode('utf-8', 'ignore') for c in test_contents)
-        print("Name : {} \n".format(self.name))
+        print("Name : {}\n".format(self.name))
         print("Number of Pages : {}".format(self.num_pages))
 
-        return all((re.search('\w{4}', c, re.UNICODE) for c in test_contents))
+        return all((re.search('[^\W\d_]{3}', c, re.UNICODE)
+                    for c in test_contents))
 
     def copy(self, output_dir):
         pdf_copy = os.path.join(output_dir, self.name)
@@ -99,7 +106,7 @@ if __name__ == '__main__':
     start_time = time.time()
     import sys
     target_dir, output_dir = sys.argv[1:]
-    max_scanned_pages, test_length = 8, 100
+    max_scanned_pages, test_length = 8, 50
     scanner = PDFScanner(max_scanned_pages=max_scanned_pages,
                          test_length=test_length)
     scanner.scan(target_dir, output_dir)
