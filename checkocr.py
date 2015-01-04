c912858b55581c4b8ac419f1eca31590a3e9970c
chrisdeveloper <chris@chrisdeveloper.com>
2015-01-04 10:50:28 +0100

    Remove dependency to PyPFD2 library to check whether number of pages of a pdf file is large (no need to use PyPDF2.PdfFileReader.numPages). Use a simple try/except block with subprocess.check_output instead. Make the program up to 4 or 5 times faster.

--- checkocr.py
+++ checkocr.py
@@ -6,29 +6,15 @@ import shutil
 import fnmatch
 import time
 
-from PyPDF2 import PdfFileReader
-
-import warnings
-warnings.filterwarnings('ignore', module="PyPDF2")
-### supress PdfReadWarning from the PyPDF2.PdfFileReader module
-
 from colorama import Fore
 
 
-class PDF(PdfFileReader):
+class PDF(object):
 
     def __init__(self, directory, name):
-        print("--------------------------------------------------------------")
         self.name = name
         self.directory = directory
         self.pathname = os.path.join(directory, name)
-        try:
-            super(self.__class__, self).__init__(self.pathname)
-            self.num_pages = self.numPages
-            # assign PdfFileReader.numPages to num_pages
-        except (AssertionError, ValueError) as e:
-            print(repr(e))
-            self.num_pages = 0
 
     def extract_content(self, first_page, last_page):
         cmd = ['pdftotext', '-q',
@@ -45,13 +31,15 @@ class PDF(PdfFileReader):
         ### Indeed, those pdfs might have ocr, but only after the first few
         ### pages, and the other way round. So it's better to start scanning
         #### longer pdfs from a more distant page number.
-
-        if self.num_pages <= treshold:
-            content = self.extract_content(first_page=1,
-                                           last_page=max_scanned_pages)
-        else:
+        try:
             content = self.extract_content(first_page=2*max_scanned_pages+1,
                                            last_page=treshold)
+            self.is_large = True
+        except subprocess.CalledProcessError:
+            content = self.extract_content(first_page=1,
+                                           last_page=max_scanned_pages)
+            self.is_large = False
+        print("------------------------------------------------------------")
         warning = "PDF compression, OCR, web optimization using \
 a watermarked evaluation copy of CVISION PDFCompressor\n\n"
         if content.decode('utf-8', 'ignore').startswith(warning):
@@ -63,17 +51,16 @@ a watermarked evaluation copy of CVISION PDFCompressor\n\n"
             print(Fore.RED + warning + "\n")
             return
         l = test_length
-        if (len(content) <= 6*l or 0 < self.num_pages <= treshold):
+        if (len(content) <= 6*l or not self.is_large):
             ### Condition deals with the special case of small pdfs or pdfs
             ### whose content can't be split into 6 parts of length l (even
             ### though they might have a high number of pages, which is typical
             ### of non-ocr ebooks).
             content = content.decode('utf-8', 'ignore')
             print("Name : {}\n".format(self.name))
-            print("Number of Pages : {}".format(self.num_pages))
+            print("Number of Pages : {}".format(self.is_large))
             return re.search('[^\W\d_]{5}', content, re.UNICODE)
             ### the re.UNICODE flag is needed for Python 2.7
-
         test_contents = [content[0:l],
                          content[l:2*l],
                          content[2*l:3*l],
@@ -82,7 +69,7 @@ a watermarked evaluation copy of CVISION PDFCompressor\n\n"
                          content[-l:]]
         test_contents = (c.decode('utf-8', 'ignore') for c in test_contents)
         print("Name : {}\n".format(self.name))
-        print("Number of Pages : {}".format(self.num_pages))
+        print("Number of Pages : {}".format(self.is_large))
         return all((re.search('[^\W\d_]{3}', c, re.UNICODE)
                     for c in test_contents))
 
