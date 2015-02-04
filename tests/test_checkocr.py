bd27a743931a4c52ed71d07919d84acf5192b69d
chrisdeveloper <chris@chrisdeveloper.com>
2015-02-04 16:28:08 +0100

    Create boilerplate test

---
+++ tests/test_checkocr.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_checkocr
+----------------------------------
+
+Tests for `checkocr` module.
+"""
+
+import unittest
+
+from checkocr import checkocr
+
+
+class TestCheckocr(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
